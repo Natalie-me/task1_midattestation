f693532f5a3439999d0010a9e1fd20e61d55ce5f
Natalie <nataliyazhandarova@gmail.com>
2023-02-24 17:38:23 +0300

    new commit

--- Fence.java
+++ Fence.java
@@ -2,21 +2,7 @@ import java.util.Scanner;
 
 public class Fence {
     public static void main(String[] args) {
-        int three_letters = 62;
-        int space = 12;
-        int phrase = (three_letters * 5) + (space * 3);
 
-        System.out.println("Enter the length of the fence");
-        Scanner sc = new Scanner(System.in);
-        int fence_length = sc.nextInt();
-
-
-
-        if (fence_length >= phrase) {
-            System.out.println("The leng of the fence is enough for the phrase");
-        } else {
-            System.out.println("The leng of the fence is not enough for the phrase");
-        }
 
     }
 }
